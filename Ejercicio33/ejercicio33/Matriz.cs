7ecd03db6b018322a2012f09d276bb0546469320
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-03-13 22:33:21 -0500

    ejercicio33

---
+++ Ejercicio33/ejercicio33/Matriz.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio33
+{
+    class Matriz
+    {
+        public int Filas { get; }
+        public int Columnas { get; }
+        private int[,] Datos;
+
+        // Constructor
+        public Matriz(int filas, int columnas)
+        {
+            Filas = filas;
+            Columnas = columnas;
+            Datos = new int[filas, columnas];
+        }
+
+        // Método para llenar la matriz con una fórmula específica
+        public void Llenar(Func<int, int, int> formula)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                for (int j = 0; j < Columnas; j++)
+                {
+                    Datos[i, j] = formula(i, j);
+                }
+            }
+        }
+
+        // Método para imprimir la matriz
+        public void Imprimir()
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                for (int j = 0; j < Columnas; j++)
+                {
+                    Console.Write(Datos[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Método para multiplicar dos matrices
+        public static Matriz Multiplicar(Matriz A, Matriz B)
+        {
+            if (A.Columnas != B.Filas)
+            {
+                throw new InvalidOperationException("Las matrices no son multiplicables.");
+            }
+
+            Matriz C = new Matriz(A.Filas, B.Columnas);
+
+            for (int i = 0; i < A.Filas; i++)
+            {
+                for (int j = 0; j < B.Columnas; j++)
+                {
+                    int suma = 0;
+                    for (int k = 0; k < A.Columnas; k++)
+                    {
+                        suma += A.Datos[i, k] * B.Datos[k, j];
+                    }
+                    C.Datos[i, j] = suma;
+                }
+            }
+
+            return C;
+        }
+    }
+}
---
+++ Ejercicio33/ejercicio33/Program.cs
@@ -0,0 +1,36 @@
+﻿using ejercicio33;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Solicitar los valores de m, n y p
+        Console.Write("Ingrese el valor de m: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el valor de n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el valor de p: ");
+        int p = int.Parse(Console.ReadLine());
+
+        // Crear objetos Matriz
+        Matriz A = new Matriz(m, n);
+        Matriz B = new Matriz(n, p);
+
+        // Llenar las matrices con sus respectivas fórmulas
+        A.Llenar((i, j) => (i + 1) * j);
+        B.Llenar((i, j) => i * (j + 1));
+
+        // Multiplicar matrices
+        Matriz C = Matriz.Multiplicar(A, B);
+
+        // Imprimir matrices
+        Console.WriteLine("\n*** A ***");
+        A.Imprimir();
+
+        Console.WriteLine("\n*** B ***");
+        B.Imprimir();
+
+        Console.WriteLine("\n*** C ***");
+        C.Imprimir();
+    }
+}
